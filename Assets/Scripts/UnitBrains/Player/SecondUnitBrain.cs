b8b1b1692e820abf67d0b0160f28c658d1df6d4a
RabbleRouserOne <Martyboom@mail.ru>
2024-10-24 04:06:19 +0300

    DZ6stolen

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
-using System.Data;
-using GluonGui.Dialog;
+using Codice.Client.Common.GameUI;
+using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
-using UnityEngine.UIElements;
-using Utilities;
 using static UnityEngine.GraphicsBuffer;
 
 namespace UnitBrains.Player
@@ -12,85 +10,101 @@ namespace UnitBrains.Player
     public class SecondUnitBrain : DefaultPlayerUnitBrain
     {
         public override string TargetUnitName => "Cobra Commando";
-        private const float OverheatTemperature = 3f; 
-        private const float OverheatCooldown = 2f; 
+        private const float OverheatTemperature = 3f;
+        private const float OverheatCooldown = 2f;
         private float _temperature = 0f;
-        private float _cooldownTime = 0f; 
+        private float _cooldownTime = 0f;
         private bool _overheated;
-
-        List<Vector2Int> outsideTarget = new List<Vector2Int>(); // Новое поле для целей вне зоны досягаемости, но которые ближе к базе, к ним и идем
-        Vector2Int closesTarget = Vector2Int.zero;  // данная переменная будет хранить ближайшую цель //условеый pos
-        List<Vector2Int> result = new List<Vector2Int>();
-
+        private List<Vector2Int> dangerousTarget = new List<Vector2Int>();
 
 
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
-            float overheatTemperature = OverheatTemperature; 
-            GetTemperature();
-            
-            if (GetTemperature() <= overheatTemperature)
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    var projectile = CreateProjectile(forTarget);
-                    AddProjectileToList(projectile, intoList);
-                }
-            }
-            else
+            float overheatTemperature = OverheatTemperature;
+
+            float _temperature = GetTemperature();
+
+            if (_temperature >= overheatTemperature)
             {
                 return;
+            }
+
+            if (_temperature == 0)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+
+            for (int i = 1; i <= _temperature; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
             }
 
             IncreaseTemperature();
         }
 
-        public override Vector2Int GetNextStep() //Этот метод возвращает следующий шаг агента. Однако, в данном случае метод просто вызывает метод GetNextStep из базового класса и возвращает его результат.
+        public override Vector2Int GetNextStep()
         {
-            Vector2Int targetPosition;
-            targetPosition = result.Count > 0 ? result[0] : unit.Pos;
-            return IsTargetInRange(targetPosition) ? unit.Pos : unit.Pos.CalcNextStepTowards(targetPosition);
-            //return base.GetNextStep();
+            if (dangerousTarget == null)
+            {
+                return Vector2Int.zero;
+            }
+            return base.GetNextStep();
         }
 
-        protected override List<Vector2Int> SelectTargets()//------------------------------------------------------------------
+        protected override List<Vector2Int> SelectTargets()
         {
-            //------
-            var allTargets = GetAllTargets();// получаем данные по всем целям через метод 
 
-            float minDistance = float.MaxValue; //смотрим ближайшую цель за счет расчета максимального расстояния 
+            float minTarget = float.MaxValue;
+            Vector2Int closestTarget = new Vector2Int();
 
-            foreach (Vector2Int target in allTargets)
+            List<Vector2Int> result = new List<Vector2Int>();
+
+
+            IEnumerable<Vector2Int> allTargets = GetAllTargets();
+
+            if (allTargets == null)
             {
-                float distance = DistanceToOwnBase(target); // проверяем ближайшую цель к базе и записываем ее как самую опасную 
-                if (distance < minDistance) // если расстояние больше мин
+                result.Clear();
+                result[0] = runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId];
+                Debug.Log(result);
+            }
+
+            foreach (var target in allTargets)
+            {
+
+                float distance = DistanceToOwnBase(target);
+                if (distance < minTarget)
                 {
-                    minDistance = distance;
-                    closesTarget = target;
+                    minTarget = distance;
+                    closestTarget = target;
+                    dangerousTarget.Clear();
+
                 }
             }
 
-            if (minDistance < float.MaxValue) // После завершения цикла проверяется, было ли найдено хотя бы одно расстояние (т.е. minDistance все еще меньше максимального значения float.MaxValue).
+            if (!IsTargetInRange(closestTarget))
             {
-                result.Clear();  // Если да, то список result очищается, и в него добавляется ближайшая цель (closesTarget).
-                result.Add(closesTarget); 
-            }
-            else
-            {
-                outsideTarget.Add(closesTarget); // если цель в не зоне видимости, то целью будут враги, которые ближе к базе
+                return new List<Vector2Int>();
             }
 
-            return result; // возвращаем значение. 
+            if (minTarget != float.MaxValue)
+            {
+                dangerousTarget.Add(closestTarget);
+                result.Add(closestTarget);
+                return result;
 
-
-    }//-------------------------------------------------------------------------------------------------------------------------------
-
-        public override void Update(float deltaTime, float time) // Этот метод обновляет состояние агента в зависимости от времени.
+            }
+            result.Clear();
+            return new List<Vector2Int>();
+        }
+        public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -100,13 +114,13 @@ namespace UnitBrains.Player
             }
         }
 
-        private int GetTemperature() 
+        private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
 
-        private void IncreaseTemperature() 
+        private void IncreaseTemperature()
         {
             _temperature += 1f;
             if (_temperature >= OverheatTemperature) _overheated = true;
